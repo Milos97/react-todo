f459bc6e68a56b72a90a8d3974f6427984848fce
Milos Bogosavljevic <milosnkbogosavljevic@gmail.com>
2020-01-21 23:12:23 +0100

    Updates

---
+++ precache-manifest.cb02418bf36b2b6794e3605a3231ea11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384665fb94c9f19cccd8cb8e2cfd6e6b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "f663024d78ae54161275",
+    "url": "/react-todo/static/css/main.aafd9a05.chunk.css"
+  },
+  {
+    "revision": "b266fa4e409875609b4e",
+    "url": "/react-todo/static/js/2.d2328538.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.d2328538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f663024d78ae54161275",
+    "url": "/react-todo/static/js/main.ad942a80.chunk.js"
+  },
+  {
+    "revision": "6821c8abf30b581997d9",
+    "url": "/react-todo/static/js/runtime-main.d9f2c231.js"
+  }
+]);
